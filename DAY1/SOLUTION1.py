d118618a334ddfcecb48f36749225ad82e9e1209
fudgemenot <komalt711@gmail.com>
2024-11-18 20:01:45 +0530

    Added solution for challenge 1

---
+++ DAY1/SOLUTION1.py
@@ -0,0 +1,8 @@
+ans="y"
+while ans=="y" or ans=="Y":
+    a=input("enter a word:")
+    if a.isalpha()==True:
+        print(a,a)
+    else:
+        print("This function do not support alphanumeric/numeric inputs")
+    ans= input("to enter more word press y or Y,to exit press any other key:")
