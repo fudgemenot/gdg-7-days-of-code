c79f2f30f7bcfac74d1df0ac39f94efec86a7a84
fudgemenot <komalt711@gmail.com>
2024-11-19 16:24:06 +0530

    added solution for day2

---
+++ Solutions/solution2.py
@@ -0,0 +1,10 @@
+ans="y"
+while ans.lower()=="y":
+    N=int(input("Enter the number of lines you want in the pyramid:"))
+    for i in range(1,N+1):
+        for j in range(1,(N+1)-i):
+          print(" ",end="")
+        for k in range(1,2*i):
+          print("*",end="")
+        print()    
+    ans=input("Press 'Y' or 'y' to continue, otherwise press any key to exit:" )
